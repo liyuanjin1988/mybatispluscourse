ba599fb3a3da30a683ca6850724f22421636605f
liyuanjin <1555322@qq.com>
2019-02-26 22:17:37 +0800

    Revert "nima"
    This reverts commit 1bcc9d6c

--- mp-04-codegenerator/src/test/java/com/bjlemon/TestJason.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/TestJason.java
@@ -3,5 +3,7 @@ package com.bjlemon;
 public class TestJason {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("haha");
+        System.out.println("boss is a dog!");
     }
 }
---
+++ mp-04-codegenerator/src/test/java/com/bjlemon/TestJasonOk.java
@@ -0,0 +1,9 @@
+package com.bjlemon;
+
+public class TestJasonOk {
+    public static void main(String[] args) {
+        System.out.println("I'am Jason too");
+        System.out.println("11111111111111");
+        System.out.println("22222222222222");
+    }
+}
