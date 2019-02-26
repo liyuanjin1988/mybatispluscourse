b87749164ef1145ee07aef7dc938727c4c0843a5
liyuanjin <1555322@qq.com>
2019-02-26 22:18:38 +0800

    333

--- mp-04-codegenerator/src/test/java/com/bjlemon/TestJasonOk.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/TestJasonOk.java
@@ -4,6 +4,6 @@ public class TestJasonOk {
     public static void main(String[] args) {
         System.out.println("I'am Jason too");
         System.out.println("11111111111111");
-        System.out.println("22222222222222");
+        System.out.println("22222222222222333");
     }
 }
