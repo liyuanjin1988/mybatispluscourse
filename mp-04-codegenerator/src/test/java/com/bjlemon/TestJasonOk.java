f673086345298745ebf3f8def8a9f3eb7caa9f6a
liyuanjin <1555322@qq.com>
2019-03-01 08:01:07 +0800

    6666666666666666

--- mp-04-codegenerator/src/test/java/com/bjlemon/TestJasonOk.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/TestJasonOk.java
@@ -6,5 +6,7 @@ public class TestJasonOk {
         System.out.println("11111111111111");
         System.out.println("22222222222222");
         System.out.println("33333333333333");
+        System.out.println("55555555555555");
+        System.out.println("44444444444444");
     }
 }
