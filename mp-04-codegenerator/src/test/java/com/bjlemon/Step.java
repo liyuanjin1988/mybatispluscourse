33b1e3103955245cf043e177747c018041cf0f2c
liyuanjin <1555322@qq.com>
2019-03-05 21:54:43 +0800

    step3:close fridge door

--- mp-04-codegenerator/src/test/java/com/bjlemon/Step.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Step.java
@@ -4,5 +4,6 @@ public class Step {
     public static void main(String[] args) {
         System.out.println("1.open fridge door");
         System.out.println("2.put elephant into fridge");
+        System.out.println("3.close fridge door");
     }
 }
