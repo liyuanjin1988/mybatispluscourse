0268c9f0e310ad607271b80003ab39764fe79ce0
liyuanjin <1555322@qq.com>
2019-03-05 21:53:40 +0800

    step2:put elephant into fridge

--- mp-04-codegenerator/src/test/java/com/bjlemon/Step.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Step.java
@@ -3,5 +3,6 @@ package com.bjlemon;
 public class Step {
     public static void main(String[] args) {
         System.out.println("1.open fridge door");
+        System.out.println("2.put elephant into fridge");
     }
 }
