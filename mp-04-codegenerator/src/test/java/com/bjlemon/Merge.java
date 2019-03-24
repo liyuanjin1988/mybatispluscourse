84d09e3fb8324b897db8959232310a46c827e1e9
liyuanjin <1555322@qq.com>
2019-03-24 21:18:32 +0800

    add 3 to merge file on master

--- mp-04-codegenerator/src/test/java/com/bjlemon/Merge.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Merge.java
@@ -3,5 +3,6 @@ package com.bjlemon;
 public class Merge {
     public static void main(String[] args) {
         System.out.println("add 1 on master branch");
+        System.out.println("add 3 on master branch");
     }
 }
