eb0149ed921aec189751ebbbb50472cef6d97e96
liyuanjin <1555322@qq.com>
2019-03-24 21:40:46 +0800

    git commit -m "master: add 5 on new branch"

--- mp-04-codegenerator/src/test/java/com/bjlemon/Merge.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Merge.java
@@ -4,5 +4,6 @@ public class Merge {
     public static void main(String[] args) {
         System.out.println("add 1 on master branch");
         System.out.println("add 2 on new branch");
+        System.out.println("add 5 on new branch");
     }
 }
