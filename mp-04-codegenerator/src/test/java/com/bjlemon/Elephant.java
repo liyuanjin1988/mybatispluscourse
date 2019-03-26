2f51f715381720eec885f6e48352f902748d7e5d
liyuanjin <1555322@qq.com>
2019-03-26 08:08:58 +0800

    Squashed commit of the following:
    commit 575abfb18960d9c0839b9fa7b48fc98a267ef3b5
    Author: liyuanjin <1555322@qq.com>
    Date:   Tue Mar 26 08:02:03 2019 +0800
    
        3. close door
    
    commit 6dc47a7d8827d184013a9517a08e8247f299b6c8
    Author: liyuanjin <1555322@qq.com>
    Date:   Tue Mar 26 08:01:00 2019 +0800
    
        2. put elephant into fridge
    
    commit e0dbe29ecfd1c72e50b692cc61056068b6f66a7f
    Author: liyuanjin <1555322@qq.com>
    Date:   Tue Mar 26 08:00:03 2019 +0800
    
        1. open door

---
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Elephant.java
@@ -0,0 +1,9 @@
+package com.bjlemon;
+
+public class Elephant {
+    public static void main(String[] args) {
+        System.out.println("1. open door");
+        System.out.println("2. put elephant into fridge");
+        System.out.println("3. close door");
+    }
+}
