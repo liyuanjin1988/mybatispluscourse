405be6877c4b9c2885a3507759a070dfbaaeec99
liyuanjin <1555322@qq.com>
2019-03-28 08:08:03 +0800

    2.put elephant into fridge

--- mp-04-codegenerator/src/test/java/com/bjlemon/Elephant2.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Elephant2.java
@@ -3,5 +3,6 @@ package com.bjlemon;
 public class Elephant2 {
     public static void main(String[] args) {
         System.out.println("1. open door");
+        System.out.println("2. put elephant into bridge");
     }
 }
