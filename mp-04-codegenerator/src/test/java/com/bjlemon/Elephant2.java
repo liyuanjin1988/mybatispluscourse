214a190b613dd1d0ef96098ef924b5bdd9467f22
liyuanjin <1555322@qq.com>
2019-03-28 08:09:08 +0800

    3. close door

--- mp-04-codegenerator/src/test/java/com/bjlemon/Elephant2.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Elephant2.java
@@ -4,5 +4,6 @@ public class Elephant2 {
     public static void main(String[] args) {
         System.out.println("1. open door");
         System.out.println("2. put elephant into bridge");
+        System.out.println("3. close door");
     }
 }
