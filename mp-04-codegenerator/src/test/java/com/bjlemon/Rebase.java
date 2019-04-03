5ff003e6aba38a4e657e19959b71ceb64661fcb9
liyuanjin <1555322@qq.com>
2019-04-03 08:10:37 +0800

    master:add 4

--- mp-04-codegenerator/src/test/java/com/bjlemon/Rebase.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Rebase.java
@@ -3,5 +3,6 @@ package com.bjlemon;
 public class Rebase {
     public static void main(String[] args) {
         System.out.println("master:add 3");
+        System.out.println("master:add 4");
     }
 }
