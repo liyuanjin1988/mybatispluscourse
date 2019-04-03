da37b1b0ac370984b794305f977bf071f60b98ad
liyuanjin <1555322@qq.com>
2019-04-03 20:41:09 +0800

    merge :add 4

--- mp-04-codegenerator/src/test/java/com/bjlemon/Merge2.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Merge2.java
@@ -3,5 +3,6 @@ package com.bjlemon;
 public class Merge2 {
     public static void main(String[] args) {
         System.out.println("merge :add 2");
+        System.out.println("merge :add 4");
     }
 }
