72b82379c27edcc3f130870df5459d8565139296
liyuanjin <1555322@qq.com>
2019-04-03 20:39:51 +0800

    master: add 3

--- mp-04-codegenerator/src/test/java/com/bjlemon/Merge2.java
+++ mp-04-codegenerator/src/test/java/com/bjlemon/Merge2.java
@@ -3,5 +3,6 @@ package com.bjlemon;
 public class Merge2 {
     public static void main(String[] args) {
         System.out.println("master: add 1");
+        System.out.println("master: add 3");
     }
 }
